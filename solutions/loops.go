edd602bdc6d8fa672ba125805dbd23c20d57b71c
Morgan Galpin <morgangalpin@gmail.com>
2020-03-13 14:58:14 -0700

    Added the loops exercise.

---
+++ solutions/loops.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Loop practice
+//
+// Make a for loop that outputs all the numbers from 1 to 5.
+//
+// e.g. 1
+// for i := 1; i <= 5; i++ {
+// 	fmt.Println(i)
+// }
+//
+// e.g. 2
+// i := 1
+// for i <= 5 {
+// 	fmt.Println(i)
+//  i++
+// }
+//
+// Exercises:
+// 1. Change the loops so they output from 1 to 10.
+// 2. Change the loops to output 0 to 9.
+// 3. Change them so they output even numbers. i.e. 2 4 6 8 10
+// 4. Output all the odd numbers. i.e. 1 3 5 7 9
+// 5. Output powers of 2. i.e. 1 2 4 8
+
+func main() {
+	
+}
